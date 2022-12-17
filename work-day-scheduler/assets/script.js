9e5c8966760c00d0753ea16579b8c52a969e7ab8
J03B <byucrazyfan@gmail.com>
2022-12-17 15:46:50 -0600

    Added skeleton structure of js file and modified alert display

--- work-day-scheduler/assets/script.js
+++ work-day-scheduler/assets/script.js
@@ -1,23 +1,26 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Wrapping all code in a jQuery call to run once the browser renders all the html elements
+$(document).ready(function () {
+  // Save button event listener
+  $('.saveBtn').on('click', function() {
+    // Get values to save and put in localStorage
+
+    // Show the alert, then hide it after 10 seconds
+  });
+
+  // function for updating if a time block is past, present, or future
+  function pastPresentFuture() {
+    // loop over time blocks then check time and validate
+  }
+
+  // once the page renders, and proactively, update the time blocks
+  pastPresentFuture();
+  setInterval(pastPresentFuture, 10000);
+
+  // Load localStorage data for each hour block
+  for (let i = 0; i < $('.container-lg').children().length; i++) {
+    $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`));
+  }
+
+  // Displays the current date in the header of the page
+  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 });
