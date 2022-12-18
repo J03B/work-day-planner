1077057c00a6887630142e8bd4c62e4905a18896
J03B <byucrazyfan@gmail.com>
2022-12-17 22:33:30 -0600

    Save buttons store and load localStorage data

--- work-day-scheduler/assets/script.js
+++ work-day-scheduler/assets/script.js
@@ -3,8 +3,15 @@ $(document).ready(function () {
   // Save button event listener
   $('.saveBtn').on('click', function() {
     // Get values to save and put in localStorage
+    var parentHour = $(this).parent();
+    var eventData = $(parentHour).children('.description').val();
+    localStorage.setItem(parentHour.attr('id'), eventData);
 
     // Show the alert, then hide it after 10 seconds
+    $('.alert').addClass("show-alert");
+    setTimeout(function() {
+      $('.alert').removeClass("show-alert");
+    }, 10000);
   });
 
   // function for updating if a time block is past, present, or future
@@ -18,7 +25,7 @@ $(document).ready(function () {
 
   // Load localStorage data for each hour block
   for (let i = 0; i < $('.container-lg').children().length; i++) {
-    $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`));
+    $(`#hour-${i+9} .description`).val(localStorage.getItem(`hour-${i+9}`));
   }
 
   // Displays the current date in the header of the page
